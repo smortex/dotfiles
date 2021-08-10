5182bd9e280a5b2bced4da48c5954b30c8c278c1
Romain Tartière <romain@blogreen.org>
2021-08-09 17:37:47 -1000

    Add a profile to manage GnuPG configuration
    This could be used in the future to fetch a keyring, but for now we are
    mainly interested in setting a default-key and having a .gnupg directory
    where the gpg-agent-forwarding will be able to store a socket file.

---
+++ modules/profile/manifests/gnupg.pp
@@ -0,0 +1,14 @@
+class profile::gnupg (
+  String[1] $default_key = '82349A78E7C0B8070B5980FFBA4D1D955112336F',
+) {
+  file { "${fact('home')}/.gnupg":
+    ensure => directory,
+    mode   => '0700',
+  }
+
+  file_line { 'gpg.conf-default_key':
+    line  => "default-key ${default_key}",
+    match => '^default-key ',
+    path  => "${fact('home')}/.gnupg/gpg.conf",
+  }
+}
