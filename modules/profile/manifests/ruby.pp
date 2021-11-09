191d41d363e37aebafcebe4c99c7418294a134b5
Romain Tartière <romain@blogreen.org>
2021-11-09 09:42:27 -1000

    Add ruby bundler configuration

---
+++ modules/profile/manifests/ruby.pp
@@ -0,0 +1,17 @@
+class profile::ruby {
+  file { "${fact('home')}/.bundle":
+    ensure => directory,
+  }
+
+  file { "${fact('home')}/.bundle/config":
+    ensure => file,
+    content => @(CONFIG),
+      ---
+      BUNDLE_PATH: "${fact('home')}/.bundle/vendor"
+      BUNDLE_JOBS: "4"
+      BUNDLE_GEM__TEST: "rspec"
+      BUNDLE_GEM__MIT: "true"
+      BUNDLE_GEM__COC: "true"
+      | CONFIG
+  }
+}
